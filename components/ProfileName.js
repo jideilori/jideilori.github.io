b9c096f90611040b445bb5c2a7eea8ab967e5cd3
Uwakmfon Apkan <astongemmy@gmail.com>
2021-04-07 10:28:51 +0100

    Update on Travis Node Version

--- components/ProfileName.js
+++ components/ProfileName.js
@@ -1,6 +1,6 @@
 export default function ProfileName() {
     return(
-        <h1 data-aos="slide-down" className="py-4 md:py-10 px-4 md:px-0 text-center md:text-left uppercase animated animator text-white text-2xl md:text-5xl">
+        <h1 data-aos="slide-down" className="font-bold py-4 md:py-10 px-4 md:px-0 text-center md:text-left uppercase animated animator text-white text-2xl md:text-5xl">
             Uwakmfon Akpan
         </h1>
     )
