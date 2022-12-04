7a533be0cb572acda58b54062b2561ef0b67df8e
Uwakmfon Akpan <astongemmy@gmail.com>
2022-12-04 12:22:02 +0100

    Updated About section

--- components/About.js
+++ components/About.js
@@ -14,12 +14,8 @@ export default function About() {
 			<div className="w-full md:w-3/5 lg:w-4/5 p-4 md:p-8">
 				<h1 className="text-4xl text-black mb-4">About</h1>
 				<div data-aos="" className="">
-					I am an astute web developer who has been creating amazing web presence
-					for numerous brands over the years.
-					<br />
-					As a professional in this field, I takes out time to discuss in details with clients
-					about their project in order to create interactive websites with high friendly user
-					experience that is very easy to navigate.
+					With so much learning, practice, and discoveries in the tech world over the years, I have vested myself with sound and impeccable intellect covering several web development technologies and infrastructures.
+					I am an astute web developer who has been creating an amazing web presence for brands over the years. As a professional in this field, I have worked with many brands such as Designkago, Housezzy Studio, as well as individuals to transform their dreams and visions into reality. I take out time to discuss in detail with clients about their projects in order to create interactive websites with a friendly user experience that is very easy to navigate and use, while at the same time growing their business ultimately.
 				</div>
 			</div>
 
