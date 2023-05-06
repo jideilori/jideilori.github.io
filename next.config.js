a05a9f57d7b36c8cd97d9088fb0a583bd9fcf8df
Aston Gemmy <astongemmy@astongemmy.local>
2023-05-06 11:19:40 +0100

    Added config files

---
+++ next.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     // issuer: {
+  //     //   test: /\.(js|ts)x?$/,
+  //     // },
+  //     use: [
+  //       {
+  //         loader: "@svgr/webpack",
+  //         options: {
+  //           svgoConfig: {
+  //             plugins: {
+  //               removeViewBox: false,
+  //             },
+  //           },
+  //         },
+  //       },
+  //     ],
+  //   });
+  //   return config;
+  // },
+};
